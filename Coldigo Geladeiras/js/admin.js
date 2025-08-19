8d72b6cd04cb12c5f183502486ee65f7edfc7bac
Elibarg <calagabriel31@gmail.com>
2025-08-19 19:05:06 -0300

    l

--- Coldigo Geladeiras/js/admin.js
+++ Coldigo Geladeiras/js/admin.js
@@ -1,4 +1,21 @@
+//
+COLDIGO = new Object();
+
 $(document).ready(function(){
     $("header").load("/pages/adm/general/header.html");
     $("footer").load("/pages/adm/general/footer.html");
 });
+
+//
+//
+COLDIGO.carregaPagina = function(pagename){
+    //
+    $("section").empty()
+    //
+    $("section").load(pagename+"/", function(respose,status,info){
+        if(status == "error") {
+            var msg = "Houve um erro ao encontrar a página"
+            $("section").html(msg);
+        }
+    });
+}
